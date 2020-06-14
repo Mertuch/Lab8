42bc23b01ac43b422d3b3ce766ff45f4b11383e7
Mertuch <mertucha@o2.pl>
2020-06-14 17:43:20 +0200

    Lab8
    Lab8

---
+++ Assets/Cube_script3.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube_script3 : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //print(transform.position);
+        //print(transform.position.x);
+
+        //transform.position = pozycja;
+        //transform.position = new Vector3(5, 5, 5);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position += new Vector3(1, 0, 0);
+        //transform.Translate(1, 0, 0);
+
+        //transform.localScale += new Vector3(0.1f, 0, 0);
+
+        //transform.Rotate(0, 0.5f, 0);
+        transform.Rotate(0, 0, 2);
+    }
+}
---
+++ Assets/s1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class s1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+ 
+        GameObject[] kule = GameObject.FindGameObjectsWithTag("do_usuniecia");
+        for (int i = 0; i < kule.Length; i++)
+        {
+            Destroy(kule[i]);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
